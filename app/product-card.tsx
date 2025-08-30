bfceb61c0b7b19d31011bbdc581b6b1b75bf159c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-30 12:21:20 +0200

    Dynamic routes - product page

--- app/product-card.tsx
+++ app/product-card.tsx
@@ -8,41 +8,42 @@ import {
 import { formatPrice } from '@/lib/utils';
 import { Product } from './generated/prisma';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Card className='pt-0 overflow-hidden'>
-      <div className='relative aspect-video'>
-        {product.image && (
-          <Image
-            src={product.image}
-            alt={product.name}
-            className='object-cover'
-            fill
-            loading='lazy'
-            sizes='(max-width: 768px) 100vw,
-                   (max-width: 1200px) 50vw, 
-                   33vw'
-          />
-        )}
-      </div>
+    <Link href={`/product/${product.slug}`}>
+      <Card className='pt-0 overflow-hidden'>
+        <div className='relative aspect-video'>
+          {product.image && (
+            <Image
+              src={product.image}
+              alt={product.name}
+              className='object-cover'
+              fill
+              loading='lazy'
+              sizes='(max-width: 768px) 100vw, 
+                     (max-width: 1200px) 50vw, 
+                     33vw'
+            />
+          )}
+        </div>
 
-      <CardHeader>
-        <CardTitle>
-          {product.name}
-        </CardTitle>
+        <CardHeader>
+          <CardTitle>
+            {product.name}
+          </CardTitle>
 
-        <CardDescription>
-          {product.description}
-        </CardDescription>
-      </CardHeader>
+          <CardDescription>
+            {product.description}
+          </CardDescription>
+        </CardHeader>
 
-      <CardFooter>
-        <p>
-          {formatPrice(product.price)}
-        </p>
-      </CardFooter>
-    </Card>
+        <CardFooter>
+          <p>{formatPrice(product.price)}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
