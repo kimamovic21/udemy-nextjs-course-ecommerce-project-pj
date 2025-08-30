83f5dbea2b5ff611ac3ce34db8fd49a0bef78ca6
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-30 12:37:04 +0200

    Loading category & improving product page UI

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from 'next/navigation';
 import { getProductBySlug } from '@/lib/actions';
 import { formatPrice } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
 
 const ProductPage = async ({
   params,
@@ -15,19 +18,36 @@ const ProductPage = async ({
   };
 
   return (
-    <div className='container mx-auto p-4'>
-      <h1 className='text-3xl font-bold mb-6'>
-        {product.name}
-      </h1>
+    <main className='container mx-auto p-4'>
+      <Card className='max-w-3xl mx-auto '>
+        <CardContent className='p-6'>
+          <h1 className='text-3xl font-bold mb-2'>
+            {product.name}
+          </h1>
 
-      <p>
-        {product.description}
-      </p>
+          <div className='flex items-center gap-2 mb-4'>
+            <span className='font-semibold text-lg'>
+              {formatPrice(product.price)}
+            </span>
 
-      <p>
-        {formatPrice(product.price)}
-      </p>
-    </div>
+            <Badge variant='outline'>
+              {product.category?.name}
+            </Badge>
+          </div>
+
+          <Separator className='my-4' />
+
+          <div className='space-y-2'>
+            <h2 className='font-medium'>
+              Description
+            </h2>
+            <p>
+              {product.description}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   );
 };
 
