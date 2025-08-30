79605d5798bcaa10e5f93483ac7bf97bb9864bc4
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-30 14:29:58 +0200

    Product page with images

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { formatPrice, sleep } from '@/lib/utils';
 import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Separator } from '@/components/ui/separator';
+import Image from 'next/image';
 
 export const generateMetadata = async ({
   params,
@@ -50,31 +51,46 @@ const ProductPage = async ({
 
   return (
     <main className='container mx-auto p-4'>
-      <Card className='max-w-3xl mx-auto '>
-        <CardContent className='p-6'>
-          <h1 className='text-3xl font-bold mb-2'>
-            {product.name}
-          </h1>
-
-          <div className='flex items-center gap-2 mb-4'>
-            <span className='font-semibold text-lg'>
-              {formatPrice(product.price)}
-            </span>
-
-            <Badge variant='outline'>
-              {product.category?.name}
-            </Badge>
+      <Card className='max-w-3xl mx-auto'>
+        <CardContent className='p-6 grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <div className='relative rounded-lg overflow-hidden '>
+            {product.image && (
+              <Image
+                src={product.image}
+                alt={product.name}
+                fill
+                priority
+                sizes='(max-width: 768px) 100vw, 50vw'
+                className='object-cover'
+              />
+            )}
           </div>
 
-          <Separator className='my-4' />
+          <div>
+            <h1 className='text-3xl font-bold mb-2'>
+              {product.name}
+            </h1>
 
-          <div className='space-y-2'>
-            <h2 className='font-medium'>
-              Description
-            </h2>
-            <p>
-              {product.description}
-            </p>
+            <div className='flex items-center gap-2 mb-4'>
+              <span className='font-semibold text-lg'>
+                {formatPrice(product.price)}
+              </span>
+
+              <Badge variant='outline'>
+                {product.category?.name}
+              </Badge>
+            </div>
+
+            <Separator className='my-4' />
+
+            <div className='space-y-2'>
+              <h2 className='font-medium'>
+                Description
+              </h2>
+              <p>
+                {product.description}
+              </p>
+            </div>
           </div>
         </CardContent>
       </Card>
