f8dbf3e9cbe893f4baac3168ba9eb89c203b04ce
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-01 17:26:20 +0200

    Search input logic

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,14 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { SearchIcon } from 'lucide-react';
 import { Input } from '../ui/input';
 
 export function SearchInput() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const initialQuery = searchParams.get('query') ?? '';
+  const [query, setQuery] = useState(initialQuery);
+
+  useEffect(() => {
+    setQuery(initialQuery);
+  }, [initialQuery]);
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    const trimmedQuery = query.trim();
+    const params = new URLSearchParams();
+
+    if (trimmedQuery) {
+      params.set('query', trimmedQuery);
+      router.push(`/search?${params.toString()}`);
+    } else {
+      router.push('/search');
+    };
+  };
+
   return (
-    <form className='relative w-full'>
+    <form className='relative w-full' onSubmit={handleSearch}>
       <SearchIcon className='absolute w-4 h-4 text-muted-foreground left-2.5 top-1/2 -translate-y-1/2' />
       <Input
         type='search'
         placeholder='Search'
         className='pl-8'
+        value={query}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
       />
     </form>
   );
